62b0d64cf06a3b960a40d85db45b2c5ae17a411b
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-11-15 17:59:32 -0600

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
-const {src,dest} = require('gulp');
+//Importamos los datos de las liberias que vamos ocupando
+const {src,dest,watch,series} = require('gulp');
+//Exportamos SASS
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done){
-    //Identificar el Archivo principal
     src('src/scss/app.scss')
-        .pipe(sass())//Compilar Sass
-        .pipe(dest('build/css'))//Exportar o guardar en una ubicación
-
-    done();
+        .pipe(sass())// {outputStyle:'expanded'}Compilamos SASS
+        .pipe(dest('build/css/'))//Generamos los archivos
+    done()
+}
+//Funcion para que escuche cada que hacemos un modificacion en el codigo
+function dev(){
+    watch('src/scss/**/*.scss',css);
 }
 
 exports.css = css;
+exports.default = series(css, dev);
