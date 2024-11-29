47283cb0dce44dfab77b3f08c9369be7646d5186
Ernesto Valdes <vernestovaldes123@gmail.com>
2024-11-29 12:52:07 -0600

    Gulp rename

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const autoprefixer = require('autoprefixer');
 function css(done){
     src('src/scss/style.scss')
         .pipe(sass({outputStyle:'compressed'}))// {outputStyle:'expanded'}Compilamos SASS
-        .pipe(dest('build/css/'))//Generamos los archivos
+        .pipe(dest('build/css/estilos.scss'))//Generamos los archivos
     done()
 }
 //Funcion para que escuche cada que hacemos un modificacion en el codigo
